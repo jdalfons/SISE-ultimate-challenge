41d93754e39c08af0f400f3dd3a42f310915fad5
Cyr-CK <cyril.kocab@univ-lyon2.fr>
2025-03-13 00:00:03 +0100

    Updated some stuffs

--- views/application.py
+++ views/application.py
@@ -2,6 +2,7 @@ import streamlit as st
 from st_audiorec import st_audiorec
 import datetime
 import os
+from src.model.transcriber import transcribe_audio
 
 DIRECTORY = "audios"
 FILE_NAME = "audio.wav"
@@ -18,11 +19,11 @@ def application():
     
     st.markdown("---")
     
-    tab1, tab2 = st.tabs(["⬆️ Record Audio", "🔈 Realtime Audio"])
+    tab1, tab2, tab3 = st.tabs(["⬆️ Record Audio", "🔈 Realtime Audio", "📝 Transcription"])
 
     with tab1:
-        st.header("⬆️ Upload Audio")
-        st.write("Here you can record audio.")
+        st.header("⬆️ Upload Audio Record")
+        st.write("Here you can upload a pre-recorded audio.")
         audio_file = st.file_uploader("Upload an audio file", type=["wav", "mp3", "ogg"])
         if audio_file is not None:
             
@@ -31,14 +32,44 @@ def application():
                 st.success(f"Saved file: {FILE_NAME}")
 
     with tab2:
-        st.header("🔈 Realtime Audio")
-        st.write("Here you can register audio.")
+        st.header("🔈 Realtime Audio Record")
+        st.write("Here you can record an audio.")
         
         if st.button("Register", key="register-button"):
             st.success("Audio registered successfully.")
 
-        wav_audio_data = st_audiorec()
+        audio_file = st_audiorec()
 
-        if wav_audio_data is not None:
-            st.audio(wav_audio_data, format='audio/wav')
+        if audio_file is not None:
+            st.audio(audio_file, format='audio/wav')
+            
+    with tab3:
+        st.header("📝 Speech2Text Transcription")
+        st.write("Here you can get the audio transcript.")
+
+        save = st.checkbox("Save transcription to .txt", value=False, key="save-transcript")
+
+        ############################# A décommenté quand ce sera débogué
+        if st.button("Transcribe", key="transcribe-button"):
+        #     # Fonction pour transcrire l'audio
+        #     transcription = transcribe_audio(st.audio)
+
+        #     # Charger et transcrire l'audio
+        #     # audio, rate = load_audio(audio_file_path) # (re)chargement de l'audio si nécessaire
+        #     transcription = transcribe_audio(audio_file, sampling_rate=16000)
+
+        #     # Afficher la transcription
+        #     st.write("Transcription :", transcription)
+                
+
+             st.success("Audio registered successfully.")
+        #     if save:
+        #         file_path = "transcript.txt"
+    
+        #         # Write the text to the file
+        #         with open(file_path, "w") as file:
+        #             file.write(transcription)
+                
+        #         st.success(f"Text saved to {file_path}")
+
             
